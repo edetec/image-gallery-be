fdc1f8be4fd3f055a789cca22a239ac4e9988f9b
Eder Soares <eder@esss.co>
2020-08-20 17:10:34 -0300

    Add migration revision

---
+++ migrations/versions/ecfadb773bf3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ecfadb773bf3
+Revises: 
+Create Date: 2020-08-20 17:16:25.198757
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ecfadb773bf3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
